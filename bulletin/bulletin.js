719d91e03acb104af76065227a4a6b419bc49f50
icafinance <icafinance@protonmail.com>
2021-01-26 14:11:32 +0800

    Update 202101261411

--- bulletin/bulletin.js
+++ bulletin/bulletin.js
@@ -1,5 +1,29 @@
 export default{
-	"zh": `我是中文`,
-	"en": `我是英文`,
-	"zh_TW": `我是繁体`,
+	"zh": `Internal Combustion Agreement
+内燃协议的共识者您好：
+经过短时间分散的传播，ICA取得了初步的共识；为更好的社群推动，迎接2021DEFI生态的大爆发，现做如下利好调整：
+
+原V级内燃催化剂10万ICA，降低为5万ICA；并且在未来推动中，根据全球共识情况可能继续调整内燃节点门智能。
+
+原持仓10万ICA帐户：可分仓多账户以利益最大化，后续爆发筹码会很重要；当然也可以流通于市场，请您自行重置择。
+
+注：ICA初创团队致力于一个全新的商业循环试验模型，不会过多干预和破坏市场的正常发展，祝您收获财富。`,
+	"en": `Internal Combustion Agreement
+Hello, Consensus of Internal Combustion Agreement:
+After a short period of scattered dissemination, ICA has reached a preliminary consensus; in order to promote a better community and welcome the 2021 DEFI ecological explosion, the following favorable adjustments are now made:
+
+The original V-class internal combustion catalyst has 100,000 ICA, which is reduced to 50,000 ICA; and in the future, it may continue to adjust the internal combustion node door intelligence based on global consensus.
+
+ICA account with 100,000 original positions: you can split multiple accounts to maximize benefits. The subsequent burst of chips will be very important; of course, it can also be circulated in the market. Please reset it yourself.
+
+Note: The ICA start-up team is committed to a brand-new business cycle test model that will not interfere too much and disrupt the normal development of the market. I wish you wealth.`,
+	"zh_TW": `Internal Combustion Agreement
+內燃協議的共識者您好：
+經過短時間分佈式的傳播，ICA取得了初步的共識；為更好的社群推動，迎接2021DEFI生態的大爆發，現做如下利好調整：
+
+原V級內燃節點10萬ICA，降低為5萬ICA；且在未來推動中，根據全球共識情況可能繼續調整內燃節點門檻。
+
+原持倉10萬ICA賬戶：可分倉多賬戶以利益最大化，後續爆發籌碼會很重要；當然亦可流通於市場，請您自行抉擇。
+
+註：ICA初創團隊致力於一個全新的商業循環試驗模型，不會過多干預和破壞市場的正常發展，祝您收穫財富。`,
 }
